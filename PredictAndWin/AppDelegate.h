e99640827eb5b921b7d605b4ab7a54b6c6286001
osama <osamaguc@gmail.com>
2018-07-09 07:39:04 +0200

    Initial Commit

---
+++ PredictAndWin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PredictAndWin
+//
+//  Created by Osama Rabie on 09/07/2018.
+//  Copyright © 2018 Osama Rabie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PredictAndWin/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PredictAndWin
+//
+//  Created by Osama Rabie on 09/07/2018.
+//  Copyright © 2018 Osama Rabie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
